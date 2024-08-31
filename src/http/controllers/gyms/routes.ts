80ac0c14c982df79ffa1fb7bd8ba0aba62e34eb6
WilliamWJD <william.jose.wjd@gmail.com>
2024-08-31 17:32:21 -0300

    feat: criando controllers para as academia

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,8 +1,16 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { search } from './search'
+import { nearby } from './nearby'
+import { create } from './create'
 
 export async function gymsRoutes(app: FastifyInstance) {
   // TODAS AS ROTAS ABAIXO DESSE HOOK VÃO CHAMAR O MIDDLEWARE DE VERIFICAÇÃO JWT
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', search)
+  app.get('/gyums/nearby', nearby)
+
+  app.post('/gyms', create)
 }
---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,20 @@
+import { MakeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchea = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { q, page } = searchGymsQuerySchea.parse(request.params)
+
+  const searchGymsUseCase = MakeSearchGymsUseCase()
+
+  const { gyms } = await searchGymsUseCase.execute({ page, query: q })
+
+  return reply.status(201).send({
+    gyms,
+  })
+}
