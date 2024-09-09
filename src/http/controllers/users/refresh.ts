d9f8c480bde727364ee0110c58b5f32904a2ad35
WilliamWJD <william.jose.wjd@gmail.com>
2024-09-09 09:02:20 -0300

    feat: middleware de autorização

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -6,8 +6,10 @@ export async function refresh(request: FastifyRequest, reply: FastifyReply) {
       true /* valida que o usuario esta autenticado, mas n olha o authorization Bearer */,
   })
 
+  const { role } = request.user
+
   const token = await reply.jwtSign(
-    {},
+    { role },
     {
       sign: {
         sub: request.user.sub,
@@ -16,7 +18,7 @@ export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   )
 
   const refreshToken = await reply.jwtSign(
-    {},
+    { role },
     {
       sign: {
         sub: request.user.sub,
